d9ce1107dfa824861dccf40608d636ca60d71f2f
Miguel García Melgar <mikongame@gmail.com>
2026-01-17 22:27:24 +0100

    error message

--- apps/api/src/middlewares/error.ts
+++ apps/api/src/middlewares/error.ts
@@ -1,2 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
-export function errorHandler(err:any,_req:Request,res:Response,_next:NextFunction){ const status=err.status||500; const message=err.message||'Internal error'; if(status>=500) console.error(err); res.status(status).json({error:message}); }
+import { ZodError } from 'zod';
+
+export function errorHandler(err: any, _req: Request, res: Response, _next: NextFunction) {
+  if (err instanceof ZodError) {
+    return res.status(400).json({ error: err.errors });
+  }
+
+  const status = err.status || 500;
+  const message = err.message || 'Internal error';
+  if (status >= 500) console.error(err);
+  return res.status(status).json({ error: message });
+}
